59110efd3acb7064dc09ef572c3127f364b1c13d
Thiago Butignon <thiagobutignon@gmail.com>
2023-11-01 14:32:44 -0300

    feat: create product model

---
+++ src/domain/models/products.ts
@@ -0,0 +1,25 @@
+/**
+{
+    "key": "fa494ffb-0af7-4bee-985c-556e5e90721f",
+    "data_preco": "2023-08-01",
+    "cod_produto": 9999999999999,
+    "sku": "PZ0912",
+    "qtd_estoque": 10.0,
+    "desconto": 0.0,
+    "data_hora_insercao": "2023-07-01 00:00:00",
+    "data_inicio": "2023-07-01",
+    "data_fim": "2023-07-31"
+}
+ */
+
+export type Products = {
+  key: string
+  priceDate: Date
+  productCode: number
+  sku: string
+  stockQuantity: number
+  discount: number
+  insertDateAndTime: Date
+  initialDate: Date
+  finalDate: Date
+}
