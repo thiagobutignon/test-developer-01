4de1c5ec3d0f4e1b9605610fc9d9b2ed891e6d71
Thiago Butignon <thiagobutignon@gmail.com>
2023-11-01 15:24:39 -0300

    test: ensure loadproducts repository throws

--- tests/data/usecases/db-load-products.spec.ts
+++ tests/data/usecases/db-load-products.spec.ts
@@ -17,8 +17,20 @@ const makeSut = (): SutTypes => {
 
 describe('DbLoadProducts', () => {
   test('Should return a list of Products on success', async () => {
+    // Arrange
     const { sut, loadProductsRepositorySpy } = makeSut()
+    // Act
     const products = await sut.performs()
+    // Assert
     expect(products).toEqual(loadProductsRepositorySpy.result)
+  })
+
+  test('Should throw if LoadProductsRepository throws', async () => {
+    const { sut, loadProductsRepositorySpy } = makeSut()
+    jest.spyOn(loadProductsRepositorySpy, 'loadAll').mockImplementationOnce(() => { throw new Error() })
+
+    const promise = sut.performs()
+
+    await expect(promise).rejects.toThrow()
   })
 })
--- tests/domain/mocks/index.ts
+++ tests/domain/mocks/index.ts
@@ -1,2 +1,1 @@
 export * from './products-mocks'
-export * from './test-helpers'
